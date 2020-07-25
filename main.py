9128b2122a447533f7d69ff91b64cc505e06b300
Arlindo Neto <arlindosilvaneto@gmail.com>
2020-07-25 18:21:08 -0300

    [UPDATE] Adapting data to correct type

--- main.py
+++ main.py
@@ -21,5 +21,8 @@ aiohttp_cors.setup(app, defaults={
 get_address_from_string_route = app.router.add_route('GET', '/api/geolocation/get/{address}', GeoLocationController.get_location_by_address)
 app['aiohttp_cors'].add(get_address_from_string_route)
 
+get_distance_route = app.router.add_route('GET', '/api/geolocation/distance', GeoLocationController.get_distance)
+app['aiohttp_cors'].add(get_distance_route)
+
 if __name__ == '__main__':
     web.run_app(app, port=settings.SERVER_PORT)
--- services/geo_service.py
+++ services/geo_service.py
@@ -6,7 +6,12 @@ R = 6373.0
 class GeoService:
 
     @staticmethod
-    def get_distance(lat1: float, lng1: float, lat2: float, lng2: float) -> float:
+    def get_distance(lat1: str, lng1: str, lat2: str, lng2: str) -> float:
+        lat1 = radians(float(lat1))
+        lng1 = radians(float(lng1))
+        lat2 = radians (float(lat2))
+        lng2 = radians(float(lng2))
+
         dlon = lng2 - lng1
         dlat = lat2 - lat1
 
