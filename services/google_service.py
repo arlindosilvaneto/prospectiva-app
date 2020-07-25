22121e9f4fafbc81570db1158d3ef7525df8cf5e
Arlindo Neto <arlindosilvaneto@gmail.com>
2020-07-25 16:06:51 -0300

    [UPDATE] Adding google static image url

--- services/google_service.py
+++ services/google_service.py
@@ -1,6 +1,7 @@
 import requests
 import config as settings
 from typing import Dict, List
+import random
 
 class GoogleService:
     
@@ -18,9 +19,31 @@ class GoogleService:
         info = google_response.get('results')[0]
 
         location = info.get('geometry', {}).get('location', {})
+        lat = location.get('lat', 0)
+        lng = location.get('lng', 0)
 
         return {
             'address': info.get('formatted_address', ''),
-            'lat': location.get('lat', 0),
-            'lng': location.get('lng', 0)
+            'lat': lat,
+            'lng': lng,
+            'picture': GoogleService.get_address_image(lat, lng) if lat != 0 else '',
         }
+
+    @staticmethod
+    def get_address_image(lat: float, lng: float) -> str:
+        base_url = 'https://maps.googleapis.com/maps/api/staticmap?'
+
+        color = [
+            'red', 'green', 'blue', 'yellow', 'brown', 'pink'
+        ][random.randint(0, 5)]
+        
+        params = [
+            f'center={lat},{lng}',
+            'zoom=13',
+            "size=600x300",
+            "maptype=roadmap",
+            f"markers=color:{color}%7c{lat},{lng}",
+            f"key={settings.GOOGLE_API_KEY}"
+        ]
+
+        return f"{base_url}{'&'.join(params)}"
--- test/services/test_google_service.py
+++ test/services/test_google_service.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+from unittest import TestCase, mock
 from services.google_service import GoogleService
 import json
 import os
@@ -8,11 +8,18 @@ class TestGoogleService(TestCase):
         with open(os.path.join(os.getcwd(), 'test/services/fixtures/google_api_response.json')) as response:
             self.google_response = json.load(response)
 
-    def test_correct_value_translation(self):
+    @mock.patch('services.google_service.random', return_value=0)
+    def test_correct_value_translation(self, random_mock):
+        lat = -19.8854666
+        lng = -43.8537075
+
+        picture_url = GoogleService.get_address_image(lat, lng)
+
         expected = {
             'address': 'R. Santos Dumont, 742 - Vila Rica, Sabar\u00e1 - MG, 34585-050, Brazil',
-            'lat': -19.8854666,
-            'lng': -43.8537075
+            'lat': lat,
+            'lng': lng,
+            'picture': picture_url
         }
 
         result = GoogleService.translate_address_info(self.google_response)
