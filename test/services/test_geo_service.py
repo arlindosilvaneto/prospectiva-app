0c4e0a1f1e7352f04955637cd52e4bba0e15e1da
Arlindo Neto <arlindosilvaneto@gmail.com>
2020-07-25 18:21:08 -0300

    [UPDATE] Adapting data to correct type

--- test/services/test_geo_service.py
+++ test/services/test_geo_service.py
@@ -5,10 +5,10 @@ from services.geo_service import GeoService
 class TestGeoService(TestCase):
 
     def test_distance(self):
-        lat1 = radians(52.2296756)
-        lng1 = radians(21.0122287)
-        lat2 = radians(52.406374)
-        lng2 = radians(16.9251681)
+        lat1 = '52.2296756'
+        lng1 = '21.0122287'
+        lat2 = '52.406374'
+        lng2 = '16.9251681'
         
         distance = GeoService.get_distance(lat1, lng1, lat2, lng2)
 
