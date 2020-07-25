5e784fc54c2cc490b49c615dc77d07827f7d41f0
Arlindo Neto <arlindosilvaneto@gmail.com>
2020-07-25 20:30:21 -0300

    [UPDATE] Change style and distance section layout

--- static/frontend/src/components/address-card.component.tsx
+++ static/frontend/src/components/address-card.component.tsx
@@ -11,23 +11,26 @@ export const AddressCard: React.FC<AddressCardProps> = ({
     address,
 }) => {
     return (
-        <div className="address-card-wrapper">
-            <div className="address-card_address">
-                <span>Address:{' '}<span>{address.address}</span></span>
+        <>
+            {address.distance && 
+                <div className="address-card_distance">
+                    <div>|</div>
+                    <div><span>{address.distance.toFixed(2)} Kilometers</span></div>
+                    <div>|</div>
+                </div>
+            }
+            <div className="address-card-wrapper">
+                <div className="address-card_address">
+                    <span>Address:{' '}<span>{address.address}</span></span>
+                </div>
+                <div className="address-card_location">
+                    <span>Lat: <span className="small">{address.lat}</span></span>
+                    <span>Lng: <span className="small">{address.lng}</span></span>
+                </div>
+                <div className="address-card_image">
+                    <img src={address.picture} alt={address.address} />
+                </div>
             </div>
-            <div className="address-card_location">
-                <span>Lat: <span className="small">{address.lat}</span></span>
-                <span>Lng: <span className="small">{address.lng}</span></span>
-            </div>
-            <div className="address-card_image">
-                <img src={address.picture} alt={address.address} />
-            </div>
-            <div className="address-card_relative-distance">
-                {address.distance && 
-                    <span>Linear distance from previous address:{' '}
-                        <span>{address.distance.toFixed(2)}</span> Kilometers
-                    </span>}
-            </div>
-        </div>
+        </>
     )
 };
